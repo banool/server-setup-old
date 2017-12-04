b555716c80d681f37e598ee8f5cc42a1556de335
Daniel Porteous <danielporteous1@gmail.com>
2017-12-04 05:14:47 +0000

    Setup for dport.me

---
+++ dport.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# DEPENDS webserver.sh
+
+set -e
+set -o xtrace
+
+# Assuming we're starting in the directory where the script lives.
+START="$PWD"
+source variables.sh
+
+TARGET="/var/www/dport"
+CONF_NAME="dport"
+
+sudo rm -rf "$TARGET"
+sudo mkdir -p "$TARGET"
+sudo chown daniel:www-data "$TARGET"
+cd "$TARGET"
+git clone git@github.com:banool/dport-site.git .
+
+cd scripts
+./get_latest_resume.sh
+
+cd "$START"
+
+sudo cp "configs/$CONF_NAME" /etc/nginx/sites-available/
+sudo rm -f "/etc/nginx/sites-enabled/$CONF_NAME"
+sudo ln -s "/etc/nginx/sites-available/$CONF_NAME" /etc/nginx/sites-enabled/
+
+sudo systemctl restart nginx
+
+sudo certbot --nginx -n -m danielporteous1@gmail.com --agree-tos --redirect -d "$DOMAIN" -d "www.$DOMAIN"
