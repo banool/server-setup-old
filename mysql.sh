e3a546bbd42449b42395fcaa030e584514f5a52f
Daniel Porteous <danielporteous1@gmail.com>
2018-03-20 22:47:45 -0700

    Simply mysql setup in exchange for more attention required

--- mysql.sh
+++ mysql.sh
@@ -7,15 +7,4 @@ source variables.sh
 
 sudo apt-get install -y mysql-server
 
-# Instead of mysql_secure_installation, we do it manually.
-# Make sure that NOBODY can access the server without a password
-ALTER USER 'root'@'localhost' IDENTIFIED BY "'$MYSQLROOTPASSWORD'" 
-# Kill the anonymous users
-sudo mysql -e "DROP USER ''@'localhost'"
-# Because our hostname varies we'll use some Bash magic here.
-sudo mysql -e "DROP USER ''@'$(hostname)'"
-# Kill off the demo database
-sudo mysql -e "DROP DATABASE test"
-# Make our changes take effect
-sudo mysql -e "FLUSH PRIVILEGES"
-# Any subsequent tries to run queries this way will get access denied because lack of usr/pwd param
+sudo mysql_secure_installation
