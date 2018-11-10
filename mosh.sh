6b9d2229d6aee23d8979da83ad15f6e4b383951a
Daniel Porteous <danielporteous1@gmail.com>
2018-11-10 11:51:24 -0800

    Fix mosh script for 18.04

--- mosh.sh
+++ mosh.sh
@@ -1,6 +1,9 @@
-PORTS="60000-61000"
+PORTS="60000:60020"
 
-sudo apt-get install -y python-software-properties
+sudo apt-get install -y software-properties-common
+
 sudo add-apt-repository -y ppa:keithw/mosh
 sudo apt-get update
 sudo apt-get install -y mosh
+
+sudo ufw allow $PORTS/udp
